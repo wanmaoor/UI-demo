3949beaf231b66fb647191c1e6728e7328d44bc5
wanmaoor <wanmao1284879375@outlook.com>
2018-08-24 17:32:27 +0800

    用parcel打包了一个用vue写成的g-button单文件组件

---
+++ src/button.vue
@@ -0,0 +1,28 @@
+<template>
+   <button class="g-button">按钮</button>
+</template>
+<script>
+  export default {}
+</script>
+<style lang="scss">
+    .g-button{
+      height: var(--button-height);
+      font-size: var(--font-size);
+      padding: 0 1em;
+      font: inherit;
+      border-radius: var(--border-radius);
+      border: 1px solid var(--border-color);
+      background: var(--button-bg);
+      &:hover{
+      border-color: var(--border-color-hover);
+    }
+    &:active{
+      border-color: var(--button-active-bg);
+    }
+    &:focus{ //&表示当前选择器, 在这里就是g-button
+      outline: none;
+    }
+    }
+    
+    
+</style>
