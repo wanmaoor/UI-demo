484c9396d4945556a9c9b0f0e06cc2f4e7da3fdc
wanmaoor <wanmao1284879375@outlook.com>
2018-08-25 16:53:03 +0800

    将svg代码整理到icon.vue

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'; 
 import Button from './button';
+import Icon from './icon.vue';
 Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
 new Vue({
   el: '#app'
 })
--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,6 @@
 <template>
    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}"><!-- 根据iconPosition传进来的值就是它的类 -->
-    <svg v-if="icon" class="icon"><use :xlink:href="`#i-${icon}`"></use></svg>
+    <g-icon v-if="icon" :name="icon"></g-icon>
     <div class="content">
       <slot></slot><!-- 插槽 slot不能加属性-->
     </div> 
